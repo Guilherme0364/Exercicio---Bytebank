0623ddbf1cc93998daf6439abb388624aa5d8327
Guilherme0364 <guilhermedg364@outlook.com>
2025-01-10 14:37:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿testaBuscarPalavra();
+
+void testaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for(int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digita a palavra da {i+1}ª posição: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser buscada: ");
+    var busca = Console.ReadLine();
+
+    foreach(string str in arrayDePalavras) { // Ele é responsavel por percorrer especificamente um array
+        if (!str.Equals(busca))
+        {
+            break;
+        }
+        else
+        {
+            Console.Write($"A palavra {busca} foi encontrada.");
+        }
+    }
+}
